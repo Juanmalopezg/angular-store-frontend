1bab6910da75077f5634d3bd57b0035e2f911c47
Juanma López G <juanmanuel.lpzg@gmail.com>
2023-03-26 15:40:34 -0300

    Add initial header (with cart icon) and footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  cartTotal: number;
 
+  constructor() {
+    this.cartTotal = 0;
+  }
+
+  ngOnInit() {
+
+  }
+
+  openCart() {
+
+  }
 }
